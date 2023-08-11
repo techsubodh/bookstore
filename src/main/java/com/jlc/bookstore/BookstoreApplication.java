bd876029e0784785bc6b8ec8ecc28cc4eb2d947e
Subodh Singh <techsubodh@gmail.com>
2023-08-10 23:44:24 -0400

    Enable https

--- src/main/java/com/jlc/bookstore/BookstoreApplication.java
+++ src/main/java/com/jlc/bookstore/BookstoreApplication.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,6 +31,21 @@ public class BookstoreApplication implements CommandLineRunner{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	/**
+	 *  This will enable CORS so that the configured rest method will have 'Access-Control-Allow-Origin' header present in the response
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				///registry.addMapping("/rest/*").allowedOrigins("http://localhost:4200/");
+				registry.addMapping("/rest/*").allowedOrigins("*");
+				
+			}
+		};
 	}
 
 	@Override
